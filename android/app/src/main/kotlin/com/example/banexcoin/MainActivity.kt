98b67f58ca71c2bb416961e94ac12b8e0ec55164
Coboy16 <smautrap.16@gmail.com>
2025-06-18 01:39:06 -0500

    init project

---
+++ android/app/src/main/kotlin/com/example/banexcoin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.banexcoin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/core.dart
@@ -0,0 +1,5 @@
+export 'constants/constants.dart';
+export 'errors/errors.dart';
+export 'network/network.dart';
+export 'usecases/usecases.dart';
+export 'utils/utils.dart';
